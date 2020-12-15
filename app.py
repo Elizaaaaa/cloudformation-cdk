96f09cc391c34de517f88952612ba21071f12cc8
Ubuntu <ubuntu@ip-172-31-32-89.us-west-2.compute.internal>
2020-12-15 00:46:46 +0000

    install env

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import glob
 
 from aws_cdk import core
 
 from update_matrics.update_matrics_stack import UpdateMatricsStack
-
 from parse_config_file import get_alarms
 
-names, dimensions = get_alarms()
+for config in glob.glob("./configs"):
+    print(config)
+    
+#names, dimensions = get_alarms()
 props = {}
 
 app = core.App()
